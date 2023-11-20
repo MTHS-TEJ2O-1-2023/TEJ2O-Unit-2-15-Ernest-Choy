f9119db9122c6cf3e632b247d37321c84b1fa103
Ernest Choy <144264236+Ernest-Choy@users.noreply.github.com>
2023-11-20 13:12:41 -0500

    tyver

--- main.ts
+++ main.ts
@@ -14,26 +14,22 @@ let rotationTime: number = 0
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-input.onButtonPressed(Button.A, function() {
+input.onButtonPressed(Button.A, function () {
   basic.clearScreen()
-  sprite = game.createSprite(0,0)
-
+  sprite = game.createSprite(0, 0)
   // reset pixel movement
   while (rotationTime <= 3) {
     pixelLocation = 0
-
     // move pixel one
     while (pixelLocation <= 4) {
       basic.pause(500)
       sprite.move(1)
       pixelLocation++
     }
-
     // rotate sprite 90 degrees
     sprite.turn(Direction.Right, 90)
     rotationTime++
   }
-
   // end
   sprite.delete()
   basic.clearScreen()
